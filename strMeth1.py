f81ffe46b65741d0abe2e3c412fb141f48bce3dd
HanaOuerghemmi <5004@holbertonschool.com>
2022-09-06 17:54:16 +0100

    :fire:first step in python:fire:

---
+++ strMeth1.py
@@ -0,0 +1,9 @@
+a = "helloo   "
+print(a)
+b = "   hello**###"
+print(b)
+print(b.strip())
+print(b.rstrip())
+print(b.lstrip())
+print(b.lstrip("#"))
+
---
+++ strtitle.py
@@ -0,0 +1,19 @@
+b= "Hello world 2 python "
+print(b.title())
+print(b.capitalize())
+c, d , e, f = "1", "11", "111", "1111"
+print(c)
+print(d)
+print(e)
+print(f)
+
+print("add zfill(4)")
+
+print(c.zfill(4))
+print(d.zfill(4))
+print(e.zfill(4))
+print(f.zfill(4))
+
+print("change to lower in upper case")
+print(b.upper())
+print(b.lower())
